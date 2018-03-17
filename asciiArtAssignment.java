ed2a54adb7c5658ca2b9ec19361c698c3b017b32
Alex Yang <a.yang2@share.epsb.ca>
2018-03-17 15:26:47 +0000

    Added some of ascii art

--- asciiArtAssignment.java
+++ asciiArtAssignment.java
@@ -9,5 +9,8 @@ public class asciiArtAssignment
 		System.out.println("AAAAA    L        EEEEE      X  ");
 		System.out.println("A   A    L        E         X X ");
 		System.out.println("A   A    LLLLL    EEEEE    X   X");
+
+		System.out.println("\"-..___     __.='>");
+		System.out.println("`.     \"\"\"\"\"   ,'");
 	}
 }
