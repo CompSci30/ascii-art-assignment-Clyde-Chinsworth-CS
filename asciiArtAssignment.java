1a383f643aa560a16105971484d3b22ab0602d4a
Alex Yang <a.yang2@share.epsb.ca>
2018-03-17 14:58:13 +0000

    Added compiled file, first line of name

---
+++ asciiArtAssignment.java
@@ -0,0 +1,9 @@
+//Ascii Art Assignment
+//17/03/2018
+public class asciiArtAssignment
+{
+	public static void main(String args[])
+	{
+		System.out.println("  A      L        EEEEE    X   X");
+	}
+}
